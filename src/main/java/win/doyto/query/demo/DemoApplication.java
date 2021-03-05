c0336452b382ccf1677456b8c423e01acc62f5a7
Forb Yuan <f0rb@163.com>
2021-03-05 22:10:06 +0800

    add initial data

--- src/main/java/win/doyto/query/demo/DemoApplication.java
+++ src/main/java/win/doyto/query/demo/DemoApplication.java
@@ -3,7 +3,12 @@ package win.doyto.query.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import win.doyto.query.web.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"win.doyto.query.demo", "win.doyto.query.web.component"})
@@ -13,4 +18,8 @@ public class DemoApplication extends WebMvcConfigurerAdapter {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Resource
+    public void setDataSource(DataSource dataSource) {
+        new ResourceDatabasePopulator(new ClassPathResource("import.sql")).execute(dataSource);
+    }
 }
