3d83c8ea67973290c409354535d3cdae70eb886a
Forb Yuan <f0rb@163.com>
2021-03-05 22:03:27 +0800

    add doyto-query-web

--- src/main/java/win/doyto/query/demo/DemoApplication.java
+++ src/main/java/win/doyto/query/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package win.doyto.query.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import win.doyto.query.web.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan(basePackages = {"win.doyto.query.demo", "win.doyto.query.web.component"})
+public class DemoApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
