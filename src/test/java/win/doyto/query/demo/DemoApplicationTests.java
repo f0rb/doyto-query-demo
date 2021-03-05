9179a183bf6bdbb38c93ca10156f4aa542fc0191
Forb Yuan <f0rb@163.com>
2021-03-05 22:45:49 +0800

    add tests

--- src/test/java/win/doyto/query/demo/DemoApplicationTests.java
+++ src/test/java/win/doyto/query/demo/DemoApplicationTests.java
@@ -1,13 +1,37 @@
 package win.doyto.query.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-class DemoApplicationTests {
+import javax.annotation.Resource;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = DemoApplication.class)
+public abstract class DemoApplicationTests {
+
+    @Resource
+    protected MockMvc mockMvc;
+
+    protected ResultActions performAndExpectSuccess(RequestBuilder requestBuilder) throws Exception {
+        return mockMvc.perform(requestBuilder)
+                      .andDo(print())
+                      .andExpect(status().isOk())
+                      .andExpect(jsonPath("$.success").value(true));
+    }
 
 }
+
---
+++ src/test/java/win/doyto/query/demo/module/role/RoleControllerTest.java
@@ -0,0 +1,88 @@
+package win.doyto.query.demo.module.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.RequestBuilder;
+import win.doyto.query.demo.DemoApplicationTests;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ * RoleControllerTest
+ *
+ * @author f0rb on 2021-03-05
+ */
+@SuppressWarnings("squid:S1192")
+class RoleControllerTest extends DemoApplicationTests {
+
+    @Test
+    void getById() throws Exception {
+        performAndExpectSuccess(get("/role/1"))
+                .andExpect(jsonPath("$.data.roleName").value("admin"))
+        ;
+    }
+
+    @Test
+    void queryByRoleName() throws Exception {
+        performAndExpectSuccess(get("/role/?roleNameLike=vip&pageSize=2&sort=id,desc"))
+                .andExpect(jsonPath("$.data.list").isArray())
+                .andExpect(jsonPath("$.data.list.size()").value(2))
+                .andExpect(jsonPath("$.data.list[0].id").value(4))
+                .andExpect(jsonPath("$.data.total").value(3))
+        ;
+    }
+
+    @Test
+    @Rollback
+    void add() throws Exception {
+        String content = "{\"roleName\":\"test5\",\"roleCode\":\"VIP5\",\"valid\":true}";
+        RequestBuilder requestBuilder = post("/role/").content(content).contentType(MediaType.APPLICATION_JSON);
+        performAndExpectSuccess(requestBuilder);
+        performAndExpectSuccess(get("/role/?pageSize=1"))
+                .andExpect(jsonPath("$.data.total").value(6))
+        ;
+    }
+
+    @Test
+    @Rollback
+    void patchRole() throws Exception {
+        RequestBuilder requestBuilder = patch("/role/2").content("{\"roleName\":\"new role\"}").contentType(MediaType.APPLICATION_JSON);
+        performAndExpectSuccess(requestBuilder);
+        performAndExpectSuccess(get("/role/2"))
+                .andExpect(jsonPath("$.data.roleName").value("new role"))
+        ;
+    }
+
+    @Test
+    @Rollback
+    void updateRole() throws Exception {
+        RequestBuilder requestBuilder = put("/role/3").content("{\"roleName\":\"test3\",\"roleCode\":\"TEST3\"}").contentType(MediaType.APPLICATION_JSON);
+        performAndExpectSuccess(requestBuilder);
+        performAndExpectSuccess(get("/role/3"))
+                .andExpect(jsonPath("$.data.roleName").value("test3"))
+                .andExpect(jsonPath("$.data.roleCode").value("TEST3"))
+        ;
+    }
+
+    @Test
+    @Rollback
+    void deleteById() throws Exception {
+        performAndExpectSuccess(delete("/role/1"));
+        performAndExpectSuccess(get("/role/?pageSize=1"))
+                .andExpect(jsonPath("$.data.total").value(4))
+                .andExpect(jsonPath("$.data.list[0].id").value(2))
+        ;
+    }
+
+    @Test
+    @Rollback
+    void batch() throws Exception {
+        performAndExpectSuccess(post("/role/").content("[{\"roleName\":\"vip4\",\"roleCode\":\"VIP4\"},{\"roleName\":\"vip5\",\"roleCode\":\"VIP5\"}]").contentType(MediaType.APPLICATION_JSON));
+        performAndExpectSuccess(get("/role/?pageSize=5"))
+                .andExpect(jsonPath("$.data.total").value(7))
+        ;
+    }
+
+}
